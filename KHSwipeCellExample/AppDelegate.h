45904fc6a6f202f2a09f6010215c3f1a4a923fa5
Khan <khan.lau@hotmail.com>
2015-01-28 12:49:05 +0800

    Initial Commit

---
+++ KHSwipeCellExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KHSwipeCellExample
+//
+//  Created by Khan on 15-1-28.
+//  Copyright (c) 2015年 Khan.lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KHSwipeCellExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KHSwipeCellExample
+//
+//  Created by Khan on 15-1-28.
+//  Copyright (c) 2015年 Khan.lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
